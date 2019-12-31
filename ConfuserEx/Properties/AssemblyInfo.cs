48a962a0b68cd9c1b02451457fe2be57a7b93a57
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2019-12-31 00:10:32 -0800

    Initial Release v1.1

---
+++ ConfuserEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Beds Protector")]
+[assembly: AssemblyDescription("ConfuserEx Mod")]
+//[assembly: AssemblyVersion("4.5.0.0")]
+//[assembly: AssemblyFileVersion("1.1.0.0")]
+
