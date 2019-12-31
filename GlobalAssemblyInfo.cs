daba5bf646bdec8c7a2e103fb98f3c2d7c77a5ee
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2019-12-30 23:44:20 -0800

    Initial Release

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Skycloud Obfuscator v1.1")]
+[assembly: AssemblyCompany("Skycloud Technology")]
+[assembly: AssemblyCopyright("Copyright (C) K019")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyInformationalVersion("v1.1 | Skycloud-Technology")]
